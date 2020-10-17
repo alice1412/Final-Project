00cc45c73a92be76fc313d095f27341fb1bb2ac8
alice1412 <dazaiosamu67@g.ncu.edu.tw>
2020-10-17 09:49:03 +0800

    deleted frontend file (coz its too big)

--- vue-frontend/src/components/Navbar-Top-After/index.vue
+++
@@ -1,8 +0,0 @@
-<script>
-export default {
-  name: "top-bar2",
-};
-</script>
-
-<template src="./template.html"></template>
-<style src="./style.css" scoped></style>
--- vuefrontend/src/components/Navbar-Top/index.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {
-  name: "top-bar",
-};
-</script>
-<template src="./template.html"></template>
-<style src="./style.css" scoped></style>
